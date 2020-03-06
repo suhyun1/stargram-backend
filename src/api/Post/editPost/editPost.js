e5d66818781d06fd656450d74f2518e4e4fee135
suhyun1 <suhyun7079@gmail.com>
2020-03-06 23:46:00 +0900

    feat(editPost) Add resolver and change datamodel to delete post

---
+++ src/api/Post/editPost/editPost.js
@@ -0,0 +1,35 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+const DELETE = "DELETE";
+const EDIT = "EDIT";
+
+
+export default {
+    Mutation : {
+        editPost: async(_,args, {request, isAuthenticated}) => {
+            isAuthenticated(request);
+            const {id, caption, location, action } = args;
+            const {user} = request;
+            const post = await prisma.$exists.post({id, user: {id: user.id}});
+            if(post){
+                if (action == EDIT){
+                    return prisma.updatePost(
+                        {
+                            data: { caption, location },
+                            where: { id }
+                        }
+                    );
+                } else if (action == DELETE) {//post가 지워지면 like도 지워져야 함
+                    return prisma.deletePost(
+                        {id}
+                    );
+                }
+                
+            }else{
+                throw Error("You can't do that");
+        
+            }
+            
+        }
+    }
+}
