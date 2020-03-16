c4ec9dcc3086e2cf1668cfdb29d5d6d846753efe
suhyun1 <suhyun7079@gmail.com>
2020-03-16 23:36:36 +0900

    feat(auth): Change return type of the mutation

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,14 +4,15 @@ export default {
     Mutation: {
         createAccount: async(_, args) => {
             const {username, email, firstName ="", lastName="", bio=""} = args;
-            const user  = await prisma.createUser({
-                username, 
-                email,
-                firstName,
-                lastName,
-                bio
+            await prisma.createUser({
+              username,
+              email,
+              firstName,
+              lastName,
+              bio
             });
-            return user;
+            return true;
+    
         }
     }
-}
+};
