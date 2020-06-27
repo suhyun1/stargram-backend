9499ebbd3e101f371eea6abc156d52fc56987abe
suhyun1 <suhyun7079@gmail.com>
2020-06-28 00:49:16 +0900

    feat(editProfile): Add checking duplicated username

--- src/api/User/editUser/editUser.js
+++ src/api/User/editUser/editUser.js
@@ -6,6 +6,10 @@ export default {
             isAuthenticated(request);
             const { username, email, firstName, lastName, bio, avatar} = args;
             const {user} = request;
+            const exists = await prisma.$exists.user({ username });
+            if (exists) {
+              throw Error("This username is already taken");
+            }
             return prisma.updateUser({
                 where: {id: user.id},
                 data: {username, email,firstName, lastName, bio, avatar}
