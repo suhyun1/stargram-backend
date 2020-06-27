1d3b9920774eeac25218c479b1e5d86f4e78831b
suhyun1 <suhyun7079@gmail.com>
2020-06-28 00:52:56 +0900

    feat(signUp): Add checking duplicated email

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,9 +4,13 @@ export default {
     Mutation: {
         createAccount: async(_, args) => {
             const {username, email, firstName ="", lastName="", bio=""} = args;
-            const exists = await prisma.$exists.user({username});
-            if(exists){
-                throw Error("This username is already taken");
+            const existName = await prisma.$exists.user({username});
+            if (existName) {
+              throw Error("This username is already taken");
+            }
+            const existEmail = await prisma.$exists.user({ email });
+            if (existEmail) {
+              throw Error("This email is already taken");
             }
             await prisma.createUser({
               username,
