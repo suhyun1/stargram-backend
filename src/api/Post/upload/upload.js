1c1259cac214dd9d14e7fd3c0acba09dbcee5ce8
suhyun1 <suhyun7079@gmail.com>
2020-07-09 17:59:49 +0900

    fix(upload): Add argument 'tag'

--- src/api/Post/upload/upload.js
+++ src/api/Post/upload/upload.js
@@ -5,7 +5,7 @@ export default {
         upload: async(_, args, {request, isAuthenticated}) => {
             isAuthenticated(request);
             const { user } = request;
-            const { caption, files, location } = args;
+            const { caption, files, location, tags } = args;
             const post = await prisma.createPost({
                 caption,
                 location,
@@ -24,6 +24,33 @@ export default {
                         }
                     })
             );
+            tags.forEach(
+                async tag => {
+                    const exist = await prisma.$exists.tag({ name : tag });
+                    if(exist){  //기존에 생성된 태그
+                        await prisma.updateTag({
+                            where: { name: tag },
+                            data: {
+                                posts: {
+                                    connect: {
+                                        id: post.id
+                                    }
+                                }
+                            }
+                        });
+                    }else{
+                        await prisma.createTag({
+                            name: tag,
+                            posts: {
+                                connect: {
+                                    id: post.id
+                                }
+                            }
+                        })
+                    }
+                }
+            );
+          
             return post;
         }
     }
